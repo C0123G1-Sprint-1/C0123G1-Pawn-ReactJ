66b0aeaf5d649e7ba46b99a2a423a0a27cbbeec1
tnq112 <79589849+tnq112@users.noreply.github.com>
2023-07-17 15:56:58 +0700

    DatNT add new source code create customer

---
+++ src/service/CustomerSaveService.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+
+export const save = async (customer) => {
+    try {
+        await axios.post(`http://localhost:8080/api/customer/`, {...customer},
+            {
+                headers:
+                    {
+                        'Authorization': 'Bearer ' + sessionStorage.getItem("TOKEN"),
+                        //   'Authorization': 'Bearer ' + "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJUaG9uZ0FkbWluIiwiaXNzIjoiQ29kZUphdmEiLCJpYXQiOjE2ODg1NDIxMDcsImV4cCI6MTY4ODYyODUwN30.HvyaQWe8aJdKeByZqB6_8nwVyoVa890IKdiFDMnH5g3YKIt1Leg9Ek8rqsrIVfrR1lDKeIEIttrnZv_mrUydDg"
+                    },
+            }
+        );
+    } catch (e) {
+        console.log(e)
+    }
+}
+export const update = async (customer) => {
+    try {
+        await axios.patch(`http://localhost:8080/api/customer/${customer.id}`, {...customer},
+            {
+                headers:
+                    {
+                        'Authorization': 'Bearer ' + sessionStorage.getItem("TOKEN"),
+                        //   'Authorization': 'Bearer ' + "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJUaG9uZ0FkbWluIiwiaXNzIjoiQ29kZUphdmEiLCJpYXQiOjE2ODg1NDIxMDcsImV4cCI6MTY4ODYyODUwN30.HvyaQWe8aJdKeByZqB6_8nwVyoVa890IKdiFDMnH5g3YKIt1Leg9Ek8rqsrIVfrR1lDKeIEIttrnZv_mrUydDg"
+                    },
+            }
+        );
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+export const findAllRegisterPawn = async () => {
+    try {
+        const result = await axios.get(`http://localhost:8080/api/register-pawn`)
+        return result.data
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+
+export const findByIdCustomer = async (id) => {
+    try {
+        const result = await axios.get(`http://localhost:8080/api/customer/${id}`)
+        return result.data
+    } catch (e) {
+        console.log(e)
+    }
+}
