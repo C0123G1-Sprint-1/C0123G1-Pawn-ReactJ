a6139db39fb3f0f0338fddaf4c6d9bba8201efee
thang516 <nguyenthangfa2001@gmail.com>
2023-07-19 11:25:17 +0700

    ThangND

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Profit from "./component/profit/Profit";
-import Interest from "./component/profit/Interest";
-import React from "react";
-import Liquidation from "./component/profit/Liquidation";
-import Foresee from "./component/profit/Foresee";
-import Navbars from "./component/navbar/Navbars";
-import InfoStore from "./component/profit/InfoStore";
-
 import './App.css';
-import {Routes, Route} from "react-router-dom"
+import {Routes ,Route} from "react-router-dom"
 import React from "react";
 // import {RegisterPawn} from "./component/register-pawn/RegisterPawn";
 import {List} from "./component/register-pawn/List";
@@ -19,36 +10,22 @@ import {Header} from "./component/register-pawn/Header";
 import {Footer} from "./component/register-pawn/Footer";
 import "./css/home.css"
 import {RegisterPawn} from "./component/register-pawn/RegisterPawn";
-
 function App() {
-    return (
-        <>
-            <Header/>
-            <Routes>
-                <Route path='/' element={<List/>}/>
-                <Route path="/create" element={<RegisterPawn/>}/>
-                <Route path={""} element={<Navbars/>}>
-                    <Route path={"/info-store"} element={<InfoStore/>}>
-                        <Route path={"/info-store/profit"} element={<Profit/>}>
-                            <Route path="/info-store/profit/:type" element={<Interest/>}/>
-                            <Route path="/info-store/profit/:type" element={<Liquidation/>}/>
-                            <Route path="/info-store/profit/:type" element={<Foresee/>}/>
-                        </Route>
-                        {/*   Đây là component về thông tin cửa hàng
-                     Mọi người muốn truyền tới component của mình thì có thể làm theo mẫu
-                        <Route path={"/url"} element={<Component/>}/>
-                    */}
-                    </Route>
+  return (
+    <>
+        <Header/>
+      <Routes>
+        {/*<Route  path='/create' element={<RegisterPawn/>} />*/}
+        <Route  path='/' element={<List/>} />
+        {/*<Route path="/" element={<Header/>} />*/}
+        {/*<Route path="/" element={<Footer/>} />*/}
+        {/*<Route path="/" element={<Navigate/>} />*/}
+          <Route path="/create" element={<RegisterPawn/>} />
+      </Routes>
+        <Footer/>
 
-                    {/*    Đây là các component khác trên thanh navbar
-                    <Route path={"/url các navbar"} element={<Component/>}>
-                */}
-                </Route>
-
-            </Routes>
-            <Footer/>
-        </>
-    );
+    </>
+  );
 }
 
 export default App;
