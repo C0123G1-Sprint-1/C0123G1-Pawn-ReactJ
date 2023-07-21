443b8f78a0c1a8e35e3ea2b045d41217941edbeb
Khanh280 <Khanh280>
2023-07-21 21:00:09 +0700

    Up

--- src/service/ProfitService.js
+++ src/service/ProfitService.js
@@ -1,22 +1,38 @@
 import axios from "axios";
-
+const token = localStorage.getItem('token')
 export const getAllContract = async (startDate, endDate, page, profitType) => {
     try {
-        return await axios.get("http://localhost:8080/api/employee/profit?startDate=" + startDate + "&endDate=" + endDate + "&page=" + (page || 0) + "&profitType=" + profitType);
+        return await axios.get("http://localhost:8080/api/employee/profit?startDate=" + startDate + "&endDate=" + endDate + "&page=" + (page || 0) + "&profitType=" + profitType,
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
     } catch (e) {
         return null;
     }
 }
 export const getDataChart = async (startDate, endDate, profitType) => {
     try {
-        return await axios.get("http://localhost:8080/api/employee/profit/statistics-profit?startDate=" + startDate + "&endDate=" + endDate + "&profitType=" + profitType);
+        return await axios.get("http://localhost:8080/api/employee/profit/statistics-profit?startDate=" + startDate + "&endDate=" + endDate + "&profitType=" + profitType,
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }
+        );
     } catch (e) {
         return null;
     }
 }
 export const getProfit = async (startDate, endDate, profitType) => {
     try {
-        return await axios.get("http://localhost:8080/api/employee/profit/total-profit?startDate=" + startDate + "&endDate=" + endDate + "&profitType=" + profitType);
+        return await axios.get("http://localhost:8080/api/employee/profit/total-profit?startDate=" + startDate + "&endDate=" + endDate + "&profitType=" + profitType,
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
     } catch (e) {
         return null;
     }
